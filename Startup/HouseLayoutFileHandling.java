eae9648833c7e000f4900d42321619701d0d070a
heldmc <70613399+heldmc@users.noreply.github.com>
2022-03-13 14:34:27 -0500

    Changes: fix the write method so that it compiles and program builds

--- Startup/HouseLayoutFileHandling.java
+++ Startup/HouseLayoutFileHandling.java
@@ -4,6 +4,7 @@ import javax.swing.filechooser.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.Scanner;
+import java.io.FileWriter;
 
 // This class handles the House Layout file handling functions
 // Reading the house layout
@@ -62,16 +63,41 @@ public class HouseLayoutFileHandling extends JFrame { // implements ActionListen
  //       return outHouseLayout;
     }
 
-    public int writeHouseLayout(Scanner inpfilehouseLayout, HouseLayout inpHouseLayout){  // in final product
-        int writeStatus = 0; // 0 indicates a failure ; 1 is success
+    public int writeHouseLayout(File inpFileHouseLayout, HouseLayout inpHouseLayout){  // in final product
+        int writeStatus = 1; // 0 indicates a success ; 1 is failure
+        int dim1, dim2;
+        dim1 = inpHouseLayout.getDim1();
+        dim2 = inpHouseLayout.getDim2();
+        int [][] TileArray = new int[dim1][dim2];
+        char tileValue = ' ';
+        char charFloorType = ' ';
+        int intFloorType = 0;
+        try {
+          FileWriter fw; // opens file to append data
+          fw = new FileWriter(inpFileHouseLayout,true);
+          fw.append(inpHouseLayout.getLayoutName());  // write the name of the House Layout
+          // need to convert TileArray to a series of characters
+          TileArray = inpHouseLayout.getTileArray();
+          for (int row =   0 ; row < dim1; row++) {
+              for (int column = 0; column < dim2; column++) {
+                  tileValue = (char) TileArray[row][column];
+                  fw.append(tileValue);
+              } ;
+          fw.append(" "); // new line to seperate the rows of the Tile Array
+          };
 
-        inpfilehouseLayout.
+          intFloorType = inpHouseLayout.getFloorType();
+          charFloorType = (char)intFloorType;
 
-        if (inpfilehouseLayout.hasNext()) // check to see if data is available
-        {
-            houseData = inpfilehouseLayout.nextLine();
+          fw.append(charFloorType);
+          writeStatus = 0;  // indicate write was successful
+          fw.close(); // close the file for write access
+
+        } catch (IOException e) {
+            e.printStackTrace();
+           writeStatus = 1; // indicates a failute
         }
-        return houseData;
+
         return writeStatus;
     }
 }
