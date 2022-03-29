4b3c0f3b8c8a48701c387f038c77a917e90109a5
heldmc <70613399+heldmc@users.noreply.github.com>
2022-03-29 11:49:44 -0500

    Changes: add components for the east side of screen (components / options)

--- Startup/MainHouseLayoutGUI.java
+++ Startup/MainHouseLayoutGUI.java
@@ -31,9 +31,34 @@ public class MainHouseLayoutGUI {
     JPanel houseActions = new JPanel();
     JPanel fileHandling = new JPanel();
     JPanel simulationActions = new JPanel();
+    JPanel LayoutWallDoorway = new JPanel();
+    JPanel LayoutFurniture = new JPanel();
+    JPanel LayoutFloors = new JPanel();
+    JPanel LayoutPaths = new JPanel();
+    JPanel LayoutSimualtion = new JPanel();
     JFrame MainHouseLayoutFrame = new JFrame("Clean Buddy -- General View");
     JTextArea tileText = new JTextArea("Layout Name");
     JButton layoutbtn = new JButton();
+    JButton Wallbtn = new JButton("Wall");
+    JButton Doorwaybtn = new JButton("Doorway");
+    JButton Chairbtn = new JButton("Chair");
+    JButton Tablebtn = new JButton("Table");
+    JButton Chestbtn = new JButton("Chest");
+    JButton ShagFloor = new JButton("Shag");
+    JButton HardwoodFloor = new JButton("Hardwood");
+    JButton LoopPile = new JButton("LoopPile");
+    JButton CutPile = new JButton("CutPile");
+    JButton randomPath = new JButton("Random");
+    JButton spiralPath = new JButton("Spiral");
+    JButton snakePath = new JButton("Snake");
+    JButton wallFollowPath = new JButton("Wall Follow");
+    JLabel simSpeed = new JLabel("Speed");
+    JSlider simSpeedSlider = new JSlider(0,50);
+
+    // In House Layout Panel used the grid layout
+    GridBagLayout gblHouseLayout = new GridBagLayout();
+    GridBagConstraints gblHouseLayoutConstraints = new GridBagConstraints();
+
 
     /**
      * Default constructor
@@ -42,23 +67,98 @@ public class MainHouseLayoutGUI {
      */
     MainHouseLayoutGUI(){
     // set up boarders definistion
-        Border houseTileBorder , houseLayoutBorder, houseActionsBorder, houseFileHandlingBorder, houseSimulationBorder, menuBorder ;
+        Border houseTileBorder , houseLayoutBorder, houseActionsBorder, houseFileHandlingBorder, houseSimulationBorder,
+               LayoutWallDoorwayBorder, LayoutFurnitureBorder, LayoutFloorsBorder, LayoutPathBorder, LayoutSimulationBorder, menuBorder ;
         houseTileBorder = BorderFactory.createTitledBorder("House Tiles");
-        houseLayoutBorder = BorderFactory.createTitledBorder("House Components");
+        houseLayoutBorder = BorderFactory.createTitledBorder("Components / Options");
         houseActionsBorder = BorderFactory.createTitledBorder("Actions");
         houseFileHandlingBorder = BorderFactory.createTitledBorder("File Handling");
         houseSimulationBorder = BorderFactory.createTitledBorder("Simulation");
-        menuBorder = BorderFactory.createTitledBorder("CleanBuddy");
+   //     menuBorder = BorderFactory.createTitledBorder("CleanBuddy");
+        LayoutWallDoorwayBorder = BorderFactory.createTitledBorder("Layout");
+        //LayoutWallDoorwayBorder = BorderFactory.createLineBorder(Color.blue,1);
+        LayoutFurnitureBorder = BorderFactory.createTitledBorder("Furniture");
+        LayoutFloorsBorder = BorderFactory.createTitledBorder("Floors");
+        LayoutPathBorder = BorderFactory.createTitledBorder("Vacuum Cleaning Algorithms");
+        LayoutSimulationBorder = BorderFactory.createTitledBorder("Simulation");
 
 
 
-// Add components to houseTile Panel
+
+        // Add components to houseTile Panel
             this.houseTile.add(tileText);
             this.houseTile.setBorder(houseTileBorder);
 
-            // Add components to HouseLayout Panel
-            this.houseLayout.setBorder(houseLayoutBorder);
-            this.houseLayout.add(layoutbtn);
+        // Add components to HouseLayout Panel
+        houseLayout.setBorder(houseLayoutBorder);
+        MainHouseLayoutFrame.add(houseTile,BorderLayout.WEST);
+
+        LayoutWallDoorway.setBorder(LayoutWallDoorwayBorder);
+        LayoutWallDoorway.setPreferredSize(new Dimension(300,50));
+        LayoutWallDoorway.add(Wallbtn);
+        LayoutWallDoorway.add(Doorwaybtn);
+
+        LayoutFurniture.setBorder(LayoutFurnitureBorder);
+        LayoutFurniture.setPreferredSize(new Dimension(300,50));
+        LayoutFurniture.add(Chairbtn) ;
+        LayoutFurniture.add(Tablebtn);
+        LayoutFurniture.add(Chestbtn);
+
+        LayoutFloors.setBorder(LayoutFloorsBorder);
+        LayoutFloors.setPreferredSize(new Dimension(300,50 ));
+        LayoutFloors.add(ShagFloor);
+        LayoutFloors.add(HardwoodFloor);
+        LayoutFloors.add(LoopPile);
+        LayoutFloors.add(CutPile);
+
+        LayoutPaths.setBorder(LayoutPathBorder);
+        LayoutPaths.setPreferredSize(new Dimension(300,50));
+        LayoutPaths.add(randomPath);
+        LayoutPaths.add(spiralPath);
+        LayoutPaths.add(snakePath);
+        LayoutPaths.add(wallFollowPath);
+
+        LayoutSimualtion.setBorder(LayoutSimulationBorder);
+        LayoutSimualtion.setPreferredSize(new Dimension(400,50));
+        LayoutSimualtion.add(simSpeed);
+        simSpeedSlider.setPaintLabels(true);
+        simSpeedSlider.setPaintTicks(true);
+        simSpeedSlider.setPaintTrack(true);
+        simSpeedSlider.setMajorTickSpacing(10);
+        simSpeedSlider.setMinorTickSpacing(1);
+        LayoutSimualtion.add(simSpeedSlider);
+
+  //      LayoutSimualtion.add(runSimulationButton);
+ //       LayoutSimualtion.add(stopSimulationButton);
+
+
+        houseLayout.setLayout(gblHouseLayout);
+
+
+        gblHouseLayoutConstraints.weightx = 1;
+        gblHouseLayoutConstraints.weighty = 1;
+        gblHouseLayoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+
+        gblHouseLayoutConstraints.gridx = 0;
+        gblHouseLayoutConstraints.gridy = 0;
+        houseLayout.add(LayoutWallDoorway,gblHouseLayoutConstraints);
+
+        gblHouseLayoutConstraints.gridx = 0;
+        gblHouseLayoutConstraints.gridy = 5;
+        houseLayout.add(LayoutFurniture,gblHouseLayoutConstraints);
+
+        gblHouseLayoutConstraints.gridx = 0;
+        gblHouseLayoutConstraints.gridy = 10;
+        houseLayout.add(LayoutFloors,gblHouseLayoutConstraints);
+
+        gblHouseLayoutConstraints.gridx = 0;
+        gblHouseLayoutConstraints.gridy = 15;
+        houseLayout.add(LayoutPaths,gblHouseLayoutConstraints);
+
+        gblHouseLayoutConstraints.gridx = 0;
+        gblHouseLayoutConstraints.gridy = 20;
+        houseLayout.add(LayoutSimualtion ,gblHouseLayoutConstraints);
+
 
             // Add components to Action Panel
             this.houseActions.setBorder(houseActionsBorder);
