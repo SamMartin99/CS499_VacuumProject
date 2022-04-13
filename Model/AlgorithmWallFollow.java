e6b48f34bf5beb9401913aa85bb198a77cce1eea
heldmc <70613399+heldmc@users.noreply.github.com>
2022-04-13 06:42:07 -0500

    Changes: add the class AAlgorithmWallFollow in the Model Package;

---
+++ Model/AlgorithmWallFollow.java
@@ -0,0 +1,13 @@
+package Model;
+
+import View.SimulationLayoutGUI;
+
+public class AlgorithmWallFollow {
+    // Attributes
+
+    // Default Constuctor
+    public AlgorithmWallFollow(SimulationLayoutGUI simulationlayout, TileArray inpTA) {
+        System.out.println(" in the AlgorithmWallFollow default constructor");
+    }
+
+}
